8f861d3492c34f380902026e64aea263f1620919
Rami Alhussein <Alhusseinr1@montlciar.edu>
2018-12-07 10:56:35 -0500

    adding Hard and Medium project folders

---
+++ Medium_Questions/BST/src/bst/BST.java
@@ -0,0 +1,13 @@
+package bst;
+
+public class BST {
+    
+    public int rangeSumBST(TreeNode root, int L, int R){
+        return 0;
+    }
+    
+    static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
