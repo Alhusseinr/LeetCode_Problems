363a6c38e70b15e11a8533822e1066a33ac4a5a0
Rami Alhussein <alhusseinr1@montclair.edu>
2019-01-30 13:10:04 -0500

    fixing pascals triangle

--- Easy_Questions/Pascals_Triangle/src/pascals_triangle/Pascals_Triangle.java
+++ Easy_Questions/Pascals_Triangle/src/pascals_triangle/Pascals_Triangle.java
@@ -9,16 +9,17 @@ public class Pascals_Triangle {
         
     }  
     public static void generate(int numRows, int rowIndex){
-        List<List<Integer>> allRows = new ArrayList();
-        ArrayList<Integer> row = new ArrayList();
         
-        for(int i = 0; i < numRows; i++){
-            row.add(0,1);
-            for(int j = 1; j < row.size()-1; j++){
-                row.set(j, row.get(i) + row.get(j + 1));
+        List<List<Integer>> allRows = new ArrayList(); // initialize allRows as a list of lists of integer
+        ArrayList<Integer> row = new ArrayList(); // initialize row as an Array List
+        
+        for(int i = 0; i < numRows; i++){ // for loop to go through all of numRows 
+            row.add(0,1); // At the start of every row add 1 at index 0 
+            for(int j = 1; j < row.size()-1; j++){ // for loop to go through the size of the row - 1 
+                row.set(j, row.get(j) + row.get(j + 1)); // we are adding the values to the row
             }
-            allRows.add(new ArrayList<Integer>(row));
+            allRows.add(new ArrayList<Integer>(row)); // we are adding the row into the all row array
         }
-        System.out.println(allRows);
+        System.out.println(allRows); // prints out all rows
     }
 }
