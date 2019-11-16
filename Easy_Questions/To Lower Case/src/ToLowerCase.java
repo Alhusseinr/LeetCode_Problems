3471cba04bc341941f9bdf225a3d263d7a1d31ae
Alhusseinr <Alhusseinr1@montclair.edu>
2019-11-16 18:27:57 -0500

    adding to lower case

---
+++ Easy_Questions/To Lower Case/src/ToLowerCase.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ToLowerCase {
+    public static void main(String[] args){
+        String str = "HOW ARE YOU";
+        System.out.println(str.toLowerCase());
+    }
+}
